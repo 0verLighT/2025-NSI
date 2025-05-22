4eed195557a46c9aa742abca72d4bb9fad9e50c7
0verLighT <alexandre@0verlight.com>
2025-05-22 23:45:32 +0200

    feat: Sujet 10

--- 10/sujet-10.py
+++ 10/sujet-10.py
@@ -1,3 +1,15 @@
+def recherche(tab, x):
+    for i in tab:
+        if i == x:
+            return i
+    return None
+
+
+print(recherche([2, 3, 4, 5, 6], 5))
+print(recherche([2, 3, 4, 6, 7], 5))
+
+
+
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def position_alphabet(lettre):
@@ -8,12 +20,15 @@ def cesar(message, decalage):
     '''Renvoie le message codé par la méthode de César
     pour le decalage donné'''
     resultat = ''
-    for ... in message: 
+    for c in message:
         if 'A' <= c and c <= 'Z':
-            indice = (...) % 26 
+            indice = (position_alphabet(c) + decalage) % 26
             resultat = resultat + alphabet[indice]
         else:
-            resultat = ... 
+            resultat = resultat + c
     return resultat
+
+print(cesar('BONJOUR A TOUS. VIVE LA MATIERE NSI !', 4))
+print(cesar('GTSOTZW F YTZX. ANAJ QF RFYNJWJ SXN !', -5))
 
 
