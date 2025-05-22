be69cf9309c182ac95a86b43863ed5f6f3f7a4d9
0verLighT <alexandre@0verlight.com>
2025-05-22 23:37:56 +0200

    feat: Sujet 9

--- 09/sujet-9.py
+++ 09/sujet-9.py
@@ -1,3 +1,17 @@
+def multiplication(n, f):
+    if f ==0:
+        return 0
+    elif f > 0:
+        return n + multiplication(n, f - 1)
+    else:
+        return -multiplication(n, -f)
+
+print(multiplication(3,5))
+print(multiplication(-4,-8))
+print(multiplication(-2,6))
+print(multiplication(-2,0))
+
+
 def dichotomie(tab, x):
     """
     tab : tableau d'entiers trié dans l'ordre croissant
@@ -7,13 +21,16 @@ def dichotomie(tab, x):
     debut = 0
     fin = len(tab) - 1
     while debut <= fin:
-        m = ... 
+        m = (debut + fin) // 2
         if x == tab[m]:
-            return ... 
+            return True
         if x > tab[m]:
             debut = m + 1
         else:
-            fin = ... 
-    return ... 
+            fin = m - 1
+    return False
 
 
+
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33],28))
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33],27))
