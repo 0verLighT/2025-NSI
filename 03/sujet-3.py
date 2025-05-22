334beb7fe0463bebf44d88b2510f74e5c7e507f6
0verLighT <alexandre@0verlight.com>
2025-05-22 19:31:33 +0200

    feat: Sujet 3

--- 03/sujet-3.py
+++ 03/sujet-3.py
@@ -1,14 +1,31 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(25))
+print(fibonacci(1000))
+
+
 def eleves_du_mois(eleves, notes):
     note_maxi = 0
-    meilleurs_eleves = ... 
+    meilleurs_eleves = []
 
-    for i in range(...): 
-        if notes[i] == ...: 
-            meilleurs_eleves.append(...) 
+    for i in range(len(eleves)):
+        if notes[i] == note_maxi:
+            meilleurs_eleves.append(eleves[i])
         elif notes[i] > note_maxi:
-            note_maxi = ... 
-            meilleurs_eleves = [...] 
+            note_maxi = notes[i]
+            meilleurs_eleves = [eleves[i]]
 
     return (note_maxi, meilleurs_eleves)
+eleves_nsi = ['a','b','c','d','e','f','g','h','i','j']
+notes_nsi = [30, 40, 80, 60, 58, 80, 75, 80, 60, 24]
+print(eleves_du_mois(eleves_nsi, notes_nsi))
 
 
