23161f0bf6542cc785889a6f78c4ddae2ba3d299
0verLighT <alexandre@0verlight.com>
2025-05-23 23:11:02 +0200

    feat: Sujet 11

--- 11/25-NSI-11.py
+++ 11/25-NSI-11.py
@@ -1,7 +1,25 @@
 arbre = (((None, 1, None), 2, (None, 3, None)),4,((None, 5, None), 6, (None, 7, None)))
 
+
 def parcours_largeur(arbre):
-  return arbre
+    if arbre is None:
+        return []
+
+    file = [arbre]
+    resultat = []
+
+    while file:
+        g, x, d = file.pop(0)
+        resultat.append(x)
+
+        if g is not None:
+            file.append(g)
+        if d is not None:
+            file.append(d)
+
+    return resultat
+
+
 parcours_largeur(arbre)
 
 
@@ -10,18 +28,18 @@ def somme_max(tab):
     sommes_max = [0]*n
     sommes_max[0] = tab[0]
     # on calcule la plus grande somme se terminant en i
-    for i in range(1,n):
-        if ... + ... > ...:
-            sommes_max[i] = ...
-        else:
-            sommes_max[i] = ...
-    # on en déduit la plus grande somme de celles-ci
-    maximum = 0
     for i in range(1, n):
-        if ... > ...:
+        if sommes_max[i - 1] + tab[i] > tab[i]:
+            sommes_max[i] = sommes_max[i - 1] + tab[i]
+        else:
+            sommes_max[i] = tab[i]
+    # on en déduit la plus grande somme de celles-ci
+    maximum = sommes_max[0]
+    for i in range(1, n):
+        if sommes_max[i] > sommes_max[maximum]:
             maximum = i
 
-    return sommes_max[...]
+    return sommes_max[maximum]
 
 print(somme_max([1, 2, 3, 4, 5]))
 print(somme_max([1, 2, -3, 4, 5]))
