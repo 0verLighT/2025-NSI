08882be055cf45ed1073c95650699c714e9a8e65
0verLighT <alexandre@0verlight.com>
2025-05-23 23:43:46 +0200

    feat!: Sujet 15

--- 15/25-NSI-15.py
+++ 15/25-NSI-15.py
@@ -1,15 +1,32 @@
+def multiplication(n, f):
+    if f ==0:
+        return 0
+    elif f > 0:
+        return n + multiplication(n, f - 1)
+    else:
+        return -multiplication(n, -f)
+
+print(multiplication(3, 5))
+print(multiplication(-4, -8))
+print(multiplication(-2, 6))
+print(multiplication(-2, 0))
 def chercher(tab, x, i, j):
     '''Renvoie l'indice de x dans tab, si x est dans tab, 
     None sinon.
     On suppose que tab est trié dans l'ordre croissant.'''
     if i > j:
         return None
-    m = (i + j) // ... 
-    if ... < x: 
-        return chercher(tab, x, ... , ...) 
+    m = (i + j) // 2
+    if tab[m] < x:
+        return chercher(tab, x, m + 1 , j)
     elif tab[m] > x:
-        return chercher(tab, x, ... , ...) 
+        return chercher(tab, x,i, m - 1)
     else:
-        return ... 
+        return m
 
 
+print(chercher([1, 5, 6, 6, 9, 12], 7, 0, 5))
+print(chercher([1, 5, 6, 6, 9, 12], 9, 0, 5))
+print(chercher([1, 5, 6, 6, 9, 12], 6, 0, 5))
+print(chercher([1], 0, 0, 0))
+print(chercher([1], 1, 0, 0))
