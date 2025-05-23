03cbedf876c52f3b081113fe1ebe386692faa3d8
0verLighT <alexandre@0verlight.com>
2025-05-23 23:57:29 +0200

    feat!: Sujet 16

--- 16/25-NSI-16.py
+++ 16/25-NSI-16.py
@@ -1,17 +1,30 @@
+def moyenne(tab):
+    s = 0
+    c = 0
+    for item in tab:
+        s = s + item[0] * item[1]
+        c = c + item[1]
+    return s / c
+print(moyenne([(15.0,2),(9.0,1),(12.0,3)]))
+
+
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+    ligne_suiv = [1]
+    for i in range(len(ligne)-1):
+        ligne_suiv.append(ligne[i+1]  + ligne[i])
+    ligne_suiv.append(1)
     return ligne_suiv
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
     triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+    for k in range(n):
+        ligne_k =  ligne_suivante(triangle[-1])
         triangle.append(ligne_k)
     return triangle
 
+print(ligne_suivante([1, 3, 3, 1]))
+print(pascal(2))
+print(pascal(3))
 
