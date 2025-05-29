8eddeeebef053bdf89b610e5b21e75df475d52f9
0verLighT <alexandre@0verlight.com>
2025-05-30 00:06:19 +0200

    feat!: Sujet 33

--- 33/25-NSI-33.py
+++ 33/25-NSI-33.py
@@ -1,17 +1,45 @@
+def insertion_abr(a, cle):
+    if a is None:
+        return (None, cle, None)
+
+    g, v, d = a
+
+    if cle == v:
+        return a
+    elif cle < v:
+        return (insertion_abr(g, cle), v, d)
+    else:
+        return (g, v, insertion_abr(d, cle))
+
+n0 = (None, 0, None)
+n3 = (None, 3, None)
+n2 = (None, 2, n3)
+abr1 = (n0, 1, n2)
+
+print(insertion_abr(abr1, 4))
+print(insertion_abr(abr1, -5))
+print(insertion_abr(abr1, 2))
+
+
 def empaqueter(liste_masses, c):
     """Renvoie le nombre minimal de boîtes nécessaires pour
     empaqueter les objets de la liste liste_masses, sachant
     que chaque boîte peut contenir au maximum c kilogrammes"""
     n = len(liste_masses)
     nb_boites = 0
-    boites = [ 0 for _ in range(n) ]
-    for masse in ...: 
+    boites = [0 for _ in range(n)]
+
+    for masse in liste_masses:
         i = 0
-        while i < nb_boites and boites[i] + ... > c: 
-            i = i + 1
+        while i < nb_boites and boites[i] + masse > c:
+            i = i +  1
         if i == nb_boites:
-            ...
-        boites[i] = ... 
-    return ... 
+            nb_boites += 1
+        boites[i] = boites[i] + masse
+
+    return nb_boites
 
 
+print(empaqueter([1, 2, 3, 4, 5], 10))
+print(empaqueter([1, 2, 3, 4, 5], 5))
+print(empaqueter([7, 6, 3, 4, 8, 5, 9, 2], 11))
