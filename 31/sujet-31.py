885f77f68b5c6d6ca2e3359f69bedde621da371b
0verLighT <alexandre@0verlight.com>
2025-05-29 23:54:03 +0200

    feat!: Sujet 30

--- 31/sujet-31.py
+++ 31/sujet-31.py
@@ -1,19 +1,35 @@
+def recherche_motif(motif, texte):
+    positions = []
+    if motif > texte:
+        return []
+    for i in range(len(texte) - len(motif) + 1):
+        if texte[i:i+len(motif)] == motif:
+            positions.append(i)
+    return positions
+
+print(recherche_motif("ab", ""))
+print(recherche_motif("ab", "cdcdcdcd"))
+print(recherche_motif("ab", "abracadabra"))
+print(recherche_motif("ab", "abracadabraab"))
+
 def parcours(adj, x, acc):
     '''Réalise un parcours en profondeur récursif
     du graphe donné par les listes d'adjacence adj 
     depuis le sommet x en accumulant les sommets
     rencontrés dans acc'''
-    if x ...: 
+    if x  not in acc:
         acc.append(x)
-        for y in ...: 
-            parcours(adj, ...) 
+        for y in adj[x]:
+            parcours(adj, y, acc)
 
 def accessibles(adj, x):
     '''Renvoie la liste des sommets accessibles dans le
     graphe donné par les listes d'adjacence adj depuis
     le sommet x.'''
     acc = []
-    parcours(adj, ...) 
+    parcours(adj, x, acc)
     return acc
 
 
+print(accessibles([[1, 2], [0, 3], [0], [1], [5], [4]], 0))
+print(accessibles([[1, 2], [0, 3], [0], [1], [5], [4]], 4))
