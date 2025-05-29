dfd5185d55d335b61cf11b7a6311b9b8e742da11
0verLighT <alexandre@0verlight.com>
2025-05-30 00:02:57 +0200

    feat!: Sujet 32

--- 32/sujet-32.py
+++ 32/sujet-32.py
@@ -1,12 +1,27 @@
+def occurrences(chercher, liste):
+    acc = 0
+    for i in range(len(liste)):
+        if liste[i] == chercher:
+            acc = acc +  1
+    return acc
+
+
+print(occurrences('e', "sciences"))
+print(occurrences('i',"mississippi"))
+print(occurrences('a',"mississippi"))
+
 valeurs = [100, 50, 20, 10, 5, 2, 1]
 
 def rendu_glouton(a_rendre, rang):
     if a_rendre == 0:
-        return ... 
+        return []
     v = valeurs[rang]
-    if v <= ...: 
-        return ... + rendu_glouton(a_rendre - v, rang) 
+    if v <= a_rendre:
+        return [v] + rendu_glouton(a_rendre - v, rang)
     else:
-        return rendu_glouton(a_rendre, ...) 
+        return rendu_glouton(a_rendre, rang + 1)
 
 
+print(rendu_glouton(67, 0))
+print(rendu_glouton(291, 0))
+print(rendu_glouton(291,1))
