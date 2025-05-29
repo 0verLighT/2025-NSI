b05d8ba38fe5f870f4ff8a3a39993c48e716067c
0verLighT <alexandre@0verlight.com>
2025-05-29 23:44:11 +0200

    feat: Sujet 30

--- 30/sujet-30.py
+++ 30/sujet-30.py
@@ -1,3 +1,11 @@
+def delta(liste):
+    resultat = [liste[0]]
+    for i in range(1, len(liste)):
+        resultat.append(liste[i] - liste[i - 1])
+    return resultat
+
+print(delta([1000, 800, 802, 1000, 1003]))
+print(delta([42]))
 class Expr:
     """Classe implémentant un arbre d'expression."""
 
@@ -18,12 +26,22 @@ class Expr:
     def infixe(self):
         """renvoie la représentation infixe de l'expression en
         chaine de caractères"""
-        s = ... 
+        s = ''
         if self.gauche is not None:
-            s = s + '(' + ... .infixe() 
-        s = s + ... 
-        if ... is not None: 
-            s = s + ... + ... 
+            s = s + '(' + self.gauche.infixe()
+        s = s + str(self.valeur)
+        if self.droite is not None:
+            s = s + self.droite.infixe() + ')'
         return s
 
+a = Expr(Expr(None, 1, None), '+', Expr(None, 2, None))
+print(a.infixe())
+b = Expr(Expr(Expr(None, 1, None), '+', Expr(None, 2, None)),
+'*', Expr(Expr(None, 3, None), '+', Expr(None, 4, None)))
+print(b.infixe())
+e = Expr(
+Expr(Expr(None, 3, None), '*', Expr(Expr(None, 8, None),
+'+', Expr(None, 7, None))),
+'-', Expr(Expr(None, 2, None), '+', Expr(None, 1, None)))
+print(e.infixe())
 
