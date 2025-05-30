0f3e4a4560cb7f43635acd56474a5e95f7899af4
0verLighT <alexandre@0verlight.com>
2025-05-30 16:21:44 +0200

    feat!: Sujet 38

--- 38/25-NSI-38.py
+++ 38/25-NSI-38.py
@@ -1,12 +1,21 @@
+def moyenne(tab):
+    somme=0
+    for i in range(len(tab)):
+        somme+=tab[i]
+    moyenne= somme/len(tab)
+    return print(moyenne)
+moyenne([1.0])
+moyenne([1.0, 2.0, 4.0])
+
 def binaire(a):
-    '''convertit un nombre entier a en sa representation 
-    binaire sous forme de chaine de caractères.'''
     if a == 0:
-        return ... 
-    bin_a = ... 
-    while ... : 
-        bin_a = ... + bin_a 
-        a = ... 
-    return bin_a
-
-
+        return '0'
+    bin_a = ''
+    while a>0 :
+        bin_a = str(a%2) + bin_a
+        a = a//2
+    return print(bin_a)
+binaire(83)
+binaire(6)
+binaire(127)
+print(binaire(0))
