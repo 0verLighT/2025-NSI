079803161384d48e5bb93f20ea0213ce6b26a7b5
0verLighT <alexandre@0verlight.com>
2025-05-30 15:54:22 +0200

    feat!: Sujet 36

--- 36/25-NSI-36.py
+++ 36/25-NSI-36.py
@@ -1,3 +1,17 @@
+def nombre_de_mots(texte):
+    acc = 0
+    for i in texte.split(' '):
+        if i == "?" or i == "!":
+            continue
+        else:
+            acc = acc + 1
+    return acc
+
+print(nombre_de_mots('Cet exercice est simple.'))
+print(nombre_de_mots('Le point d exclamation est séparé !'))
+print(nombre_de_mots('Combien de mots y a t il dans cette phrase ?'))
+print(nombre_de_mots('Fin.'))
+
 class Noeud:
     def __init__(self, etiquette):
         '''Méthode constructeur pour la classe Noeud.
@@ -11,13 +25,18 @@ class Noeud:
         en préservant sa structure.'''
         if cle < self.etiquette:
             if self.gauche != None:
-                ...
+                self.gauche.inserer(cle)
             else:
-                self.gauche = ... 
+                self.gauche = Noeud(cle)
         else:
-            ...
-                ...
+            if self.droit != None:
+                self.droit.inserer(cle)
             else:
-                ... = Noeud(cle) 
-
-
+                self.droit = Noeud(cle)
+arbre = Noeud(7)
+for cle in (3, 9, 1, 6):
+    arbre.inserer(cle)
+print(arbre.gauche.etiquette)
+print(arbre.droit.etiquette)
+print(arbre.gauche.gauche.etiquette)
+print(arbre.gauche.droit.etiquette)
