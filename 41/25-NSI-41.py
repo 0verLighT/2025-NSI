7b8f96a69de0b7965acc0ab1902a4121bf21bc82
0verLighT <alexandre@0verlight.com>
2025-05-30 16:18:32 +0200

    feat!: Sujet 41

--- 41/25-NSI-41.py
+++ 41/25-NSI-41.py
@@ -1,43 +1,43 @@
+def ou_exclusif(a,b):
+    c=[]
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            c.append(0)
+        else:
+            c.append(1)
+    return print(c)
+ou_exclusif([1, 0, 1, 0, 1, 1, 0, 1], [0, 1, 1, 1, 0, 1, 0, 0])
+ou_exclusif([1, 1, 0, 1], [0, 0, 1, 1])
+
 class Carre:
-    def __init__(self, liste, n): 
-        self.ordre = n        
-        self.tableau = [[liste[i + j * n] for i in range(n)] 
-                        for j in range(n)] 
-                                   
-    def affiche(self):        
-        '''Affiche un carré''' 
-        for i in range(self.ordre): 
-            print(self.tableau[i]) 
-                                   
-    def somme_ligne(self, i): 
-        '''Calcule la somme des valeurs de la ligne i''' 
-        somme = 0             
-                                   
-        for j in range(self.ordre): 
-            somme = somme + self.tableau[i][j] 
-        return somme          
-                                   
-    def somme_col(self, j):   
-        '''Calcule la somme des valeurs de la colonne j''' 
-        somme = 0
-                                   
-        for i in range(self.ordre): 
-            somme = somme + self.tableau[i][j] 
-        return somme          
-                                   
-                                   
-    def est_semimagique(self): 
-        s = self.somme_ligne(0) 
-        #test de la somme de chaque ligne 
-        for i in range(...): 
-            if ... != s: 
-                return ... 
-                                   
-        #test de la somme de chaque colonne 
-        for j in range(...): 
-            if ... != s: 
-                return ... 
-                                   
-        return ... 
+    def __init__(self, liste, n):
+        self.ordre = n
+        self.tableau = [[liste[i + j * n] for i in range(n)] for j in range(n)]
 
+    def affiche(self):
+        for i in range(self.ordre):
+            print(self.tableau[i])
 
+    def somme_ligne(self, i):
+        return sum(self.tableau[i])
+
+    def somme_col(self, j):
+        return sum(self.tableau[i][j] for i in range(self.ordre))
+
+    def est_semimagique(self):
+        s = self.somme_ligne(0)
+
+        # Vérifie les lignes
+        for i in range(1, self.ordre):
+            if self.somme_ligne(i) != s:
+                return False
+
+        # Vérifie les colonnes
+        for j in range(self.ordre):
+            if self.somme_col(j) != s:
+                return False
+
+        return True
+lst_c3 = [3, 4, 5, 4, 4, 4, 5, 4, 3]
+c3 = Carre(lst_c3, 3)
+c3.affiche()
