6e243b1bfb75b145d893d2e212f9a1579ba8709f
0verLighT <alexandre@0verlight.com>
2025-05-30 16:15:27 +0200

    feat!: Sujet 43

--- 43/sujet-43.py
+++ 43/sujet-43.py
@@ -1,16 +1,27 @@
+def couples_consecutifs(tab):
+    a=[]
+    for i in range(len(tab)-1):
+        if tab[i] +1 == tab[i+1]:
+            a.append((tab[i],tab[i+1]))
+    return a
+print(couples_consecutifs([1, 4, 3, 5]))
+print(couples_consecutifs([1, 4, 5, 3]))
+print(couples_consecutifs([1, 1, 2, 4]))
+print(couples_consecutifs([7, 1, 2, 5, 3, 4]))
+print(couples_consecutifs([5, 1, 2, 3, 8, -5, -4, 7]))
+
 def colore_comp1(M, i, j, val):
     if M[i][j] != 1:
         return
-
     M[i][j] = val
-
     if i-1 >= 0: # propage en haut
         colore_comp1(M, i-1, j, val)
-    if ... < len(M): # propage en bas 
-        colore_comp1(M, ..., j, val) 
-    if ...: # propage à gauche 
-        colore_comp1(M, ..., ..., val) 
-    if ...: # propage à droite 
-        ...
-
-
+    if i+1 < len(M): # propage en bas
+        colore_comp1(M, i+1, j, val)
+    if j-1 >=0: # propage à gauche
+        colore_comp1(M, i, j-1, val)
+    if j+1 <len(M[i]): # propage à droite
+        colore_comp1(M, i, j+1,val)
+        return M
+M = [[0, 0, 1, 0], [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0]]
+print(colore_comp1(M, 2, 1, 3))
