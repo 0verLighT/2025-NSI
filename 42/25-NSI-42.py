d0d180dfd65a14813af5e2e4e0e3d3b900cb159f
0verLighT <alexandre@0verlight.com>
2025-05-30 16:16:24 +0200

    feat!: Sujet 42

--- 42/25-NSI-42.py
+++ 42/25-NSI-42.py
@@ -1,13 +1,20 @@
+def nb_repetitions(elt, tab):
+    compteur = 0
+    for element in tab:
+        if element == elt:
+            compteur += 1
+    return compteur
+print(nb_repetitions(5, [2, 5, 3, 5, 6, 9, 5]))
+print(nb_repetitions('A', ['B', 'A', 'B', 'A', 'R']))
+print(nb_repetitions(12, [1, '!', 7, 21, 36, 44]))
+
 def binaire(a):
-    '''convertit un nombre entier a en sa representation 
-    binaire sous forme de chaine de caractères.'''
     if a == 0:
         return '0'
-    bin_a = ... 
-    while ...: 
-        bin_a = ... + bin_a 
-        a = ... 
+    bin_a = ''
+    while a>0:
+        bin_a = str(a%2) + bin_a
+        a = a//2
     return bin_a
-
-
-
+print(binaire(0))
+print(binaire(77))
