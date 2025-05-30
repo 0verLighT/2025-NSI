e707e019af7f2285dcf5c28fefe4ff7a2ff3b608
0verLighT <alexandre@0verlight.com>
2025-05-30 16:12:48 +0200

    feat!: Sujet 44

--- 44/sujet-44.py
+++ 44/sujet-44.py
@@ -1,44 +1,60 @@
-coeur = [[0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
-         [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
-         [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-         [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
+def moyenne(notes):
+    somme = 0
+    somme_coeff = 0
+    for i in range(len(notes)):
+        somme = somme + (notes[i][0] * notes[i][1])
+        somme_coeff += notes[i][1]
+    if somme_coeff == 0:
+        return None
+    moyenne = somme / somme_coeff
+    return moyenne
+
+print(moyenne([(8, 2), (12, 0), (13.5, 1), (5, 0.5)]))
+print(moyenne([(3, 0), (5, 0)]))
 
 def affiche(dessin):
-    ''' affichage d'une grille : les 1 sont représentés par 
-        un "*" , les 0 par une espace " " '''
+    '''Affichage d'une grille : les 1 sont représentés par un "*" , les 0 par une espace " "'''
     for ligne in dessin:
         affichage = ''
         for col in ligne:
             if col == 1:
-                affichage = affichage + "*"
+                affichage += "*"
             else:
-                affichage = affichage + " "
+                affichage += " "
         print(affichage)
 
-
-def liste_zoom(liste_depart,k):
-    '''renvoie une liste contenant k fois chaque élément de
-       liste_depart'''
-    liste_zoomee = ... 
-    for elt in ... : 
+def liste_zoom(liste_depart, k):
+    '''Renvoie une liste contenant k fois chaque élément de liste_depart'''
+    liste_zoomee = []
+    for elt in liste_depart:
         for i in range(k):
-            ...
+            liste_zoomee.append(elt)
     return liste_zoomee
 
-def dessin_zoom(grille,k):
-    '''renvoie une grille où les lignes sont zoomées k fois 
-       ET répétées k fois'''
-    grille_zoomee=[]
+def dessin_zoom(grille, k):
+    '''Renvoie une grille où les lignes sont zoomées k fois ET répétées k fois'''
+    grille_zoomee = []
     for ligne in grille:
-        ligne_zoomee = ... 
+        ligne_zoomee = liste_zoom(ligne, k)
         for i in range(k):
-            ... .append(...) 
+            grille_zoomee.append(ligne_zoomee)
     return grille_zoomee
+coeur = [
+    [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+]
 
+print("Cœur original :")
+affiche(coeur)
 
+print("\nCœur zoomé x2 :")
+affiche(dessin_zoom(coeur, 2))
+print(liste_zoom([1, 2, 3], 3))
