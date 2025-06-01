43fb4fdf137027a1a40b76e345aa6cfe93350541
0verLighT <alexandre@0verlight.com>
2025-06-01 23:54:21 +0200

    fix!: sujet 4 ex1

--- 04/sujet-4.py
+++ 04/sujet-4.py
@@ -3,7 +3,7 @@ def ecriture_binaire_entier_positif(n):
     if n == 0:
         return "0"
     while n > 0:
-        res += str(n % 2)
+        res =  str(n % 2) + res
         n = n // 2
     return res
 
